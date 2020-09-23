dce5aa5afcee73bd4a93d1e0312af7bb9caa2426
Selene Rodriguez <selene.rodriguez7472@alumnos.udg.mx>
2020-09-23 16:28:24 -0500

    Revert "Comentamos el Hola mundo" Se necesita que imprima el Hola mundo This reverts commit 2a70a40151ab0731ed1ae3cee628cd4dd9f09e85.

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main()
 {
     char nombre[100];
     unsigned int edad;
-    ///printf("Hola mundo:D\n");
+    printf("Hola mundo:D\n");
     printf("Ingrese su nombre: ");
     fgets(nombre, sizeof(nombre), stdin);
     printf("Hola %s", nombre);
